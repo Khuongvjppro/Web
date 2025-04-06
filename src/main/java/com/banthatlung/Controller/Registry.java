8ee09c9e15e206ecfb1282f1a05eab3ce45fed47
Lê Công Bình <22130026@st.hcmuaf.edu.vn>
2025-04-06 13:50:08 +0700

    Add files via upload

--- src/main/java/com/banthatlung/Controller/Registry.java
+++ src/main/java/com/banthatlung/Controller/Registry.java
@@ -1,5 +1,6 @@
 package com.banthatlung.Controller;
 
+import com.banthatlung.Dao.model.Account;
 import com.banthatlung.Dao.model.User;
 import com.banthatlung.services.AuthService;
 import jakarta.servlet.ServletException;
@@ -9,13 +10,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.SQLException;
 @WebServlet(name = "Registry", value = "/register")
 public class Registry extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/register.jsp").forward(req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
 
         AuthService authService = new AuthService();
 
@@ -25,19 +33,32 @@ public class Registry extends HttpServlet {
         String cpwd = req.getParameter("cpassword");
 
         if (!pwd.equals(cpwd)) {
-            resp.getWriter().write("Mật khẩu không khớp");
+            req.setAttribute("error", "Mật khẩu không khớp");
+            req.getRequestDispatcher("/register.jsp").forward(req, resp);
             return;
         }
+
         String hashedPassword = PasswordUtils.encryptPassword(pwd);
         User u = new User();
-        u.setUsername(uname);
+        Account account = new Account();
+        account.setUsername(uname);
         u.setEmail(fname);
-        u.setPass(hashedPassword);
+        account.setPass(hashedPassword);
 
-        if (authService.register(u)) {
-            resp.getWriter().write("Đăng ký thành công");
-        } else {
-            resp.getWriter().write("Tên tài khoản đã tồn tại");
-        }
+        try {
+			if (authService.register(account)) {
+			    req.setAttribute("success", "Đăng ký thành công, vui lòng đăng nhập!");
+			    req.getRequestDispatcher("/login.jsp").forward(req, resp);
+			} else {
+			    req.setAttribute("error", "Tên tài khoản đã tồn tại");
+			    req.getRequestDispatcher("/register.jsp").forward(req, resp);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
