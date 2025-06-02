24d16ab4fda5660c6f86401451d44a968be65038
JustBaoAh <22130008@st.hcmuaf.edu.vn>
2025-06-02 15:50:56 +0700

    fix register and login

--- src/main/java/com/banthatlung/services/AuthService.java
+++ src/main/java/com/banthatlung/services/AuthService.java
@@ -2,6 +2,7 @@ package com.banthatlung.services;
 
 import java.sql.SQLException;
 
+import com.banthatlung.Controller.PasswordUtils;
 import com.banthatlung.Dao.AccountDAO;
 import com.banthatlung.Dao.UserDao;
 import com.banthatlung.Dao.model.Account;
@@ -11,11 +12,17 @@ public class AuthService {
     public Account checkLogin(String username, String pass){
         AccountDAO dao = new AccountDAO();
         Account account = dao.findAccount(username);
-        if(account != null && pass.equals(account.getPass())){
-            return account;
+
+        if(account != null) {
+            String hashedInput = PasswordUtils.encryptPassword(pass);
+            if (hashedInput.equals(account.getPass())) {
+                return account;
+            }
         }
+
         return null;
     }
+
 
     public User findByUserId(int userId){
         UserDao dao = new UserDao();
@@ -29,7 +36,7 @@ public class AuthService {
         if(a != null) {
             return false;
         }
-        return dao.createAccount(a);
+        return dao.createAccount(account);
     }
 
     public boolean changePassword(String accountId, String oldPassword, String newPassword) throws SQLException {
